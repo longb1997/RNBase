c570a98b8892cbe08c3bcff9a7558e0b0ddb0444
longb1997 <hoanglonghaui97@gmail.com>
2021-06-11 20:23:38 +0700

    rename

--- ios/ProjectNameTests/ProjectNameTests.m
+++ ios/ProjectNameTests/ProjectNameTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface RNBaseTests : XCTestCase
+@interface ProjectNameTests : XCTestCase
 
 @end
 
-@implementation RNBaseTests
+@implementation ProjectNameTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
